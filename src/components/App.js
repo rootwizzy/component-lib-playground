04e555bd45e03c08e2c1ddcc74584fc15e7d4989
Zachary Johnson <wizzy@Sirius.hsd1.il.comcast.net>
2018-02-24 22:13:15 -0600

    ADD buttons to template

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,43 @@ import '../assets/stylesheets/base.scss';
 const App = ({ name }) => {
   return (
     <div>
+      <h1> Component Library </h1>
+      <hr></hr>
+
       <Label>Hello, {name}!</Label>
-      <Button
-        bgColor="orange"
-        size="large"
-      >
-        Button
+
+      {/* Add Button Group or non-inline default */}
+      <h2> Buttons </h2>
+      <Button bgColor="green" size="small">
+        Small Button
+      </Button>
+
+      <br></br>
+
+      <Button bgColor="yellow" fontColor="black" size="medium">
+        Medium Button
+      </Button>
+
+      <br></br>
+
+      <Button bgColor="orange" size="large">
+        Large Button
+      </Button>
+
+      <br></br>
+
+      <Button bgColor="blue" size="wide">
+        Wide Button
+      </Button>
+
+      <br></br>
+
+      <Button bgColor="blue" size="extraWide">
+        Wide Button
+      </Button>
+
+      <Button bgColor="purple" size="fullWidth">
+        Full Width Button
       </Button>
     </div>
   );
